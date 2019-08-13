b5b788084b5d009c1bedc54d2b1a7964afbeabdd
Wang Runfeng <458473502@qq.com>
2019-08-13 13:40:05 +0800

    2

--- 移动端-去哪儿网/src/pages/city/components/cityList.vue
+++ 移动端-去哪儿网/src/pages/city/components/cityList.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="list">
-    <div class="title">
-      <div class="area border-topbottom">当前城市</div>
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
       <div class="button-list">
         <div class="button-wrapper">
           <div class="button">北京</div>
         </div>
       </div>
     </div>
-    <div class="title">
-      <div class="area border-topbottom">热门城市</div>
+    <div class="area">
+      <div class="title border-topbottom">热门城市</div>
       <div class="button-list">
         <div class="button-wrapper">
           <div class="button">北京</div>
@@ -40,8 +40,41 @@
         </div>
       </div>
     </div>
-    <div class="title">
-      <div class="area border-topbottom">A</div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
     </div>
   </div>
 </template>
@@ -54,20 +87,26 @@ export default {
 
 <style lang="stylus" scoped>
 @import '~@/assets/styles/varibles.styl'
-
-.title
-  line-height: 1.375rem
-  background: #eee
-  color: #666
-  font-size: .8125rem
-  border-bottom: 1px solid #ccc
-  .area
-    padding: .6rem 0 .6rem .625rem
+.list
+  overflow: hidden
+  position: absolute
+  top: 5rem
+  left: 0
+  right: 0
+  bottom: 0
+  .title
+    line-height: 1.375rem
+    background: #eee
+    color: #666
+    font-size: .8125rem
+    border-bottom: 1px solid #ccc
+    padding: .3rem 0 .3rem .625rem
   .button-list
     overflow: hidden
     display: flex
     justify-content: space-between
     flex-wrap: wrap
+    background: #eee
     .button-wrapper
       height: 2.875rem
       width: 33%
@@ -79,4 +118,12 @@ export default {
         text-align: center
         font-size: 14px
         padding: auto 0
+  .item-list
+    .item
+      line-height: 1.6875rem
+      color: #666
+      background: #fff
+      padding-left: .625rem
+    .border-bottom
+      border-bottom: 1px solid #ccc
 </style>
